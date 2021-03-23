f611cfffb70016d643e17118f3eedb55490a7a63
Akshat Tripathi <akshat.tripathi6568@gmail.com>
2021-03-23 10:47:29 +0000

    ADDED yaml pipeline support

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ import os
 import glob
 from stage import *
 from progress.counter import Counter
-from resolution.image_super_resolution import predict
-from interpolation.interpolate import interpolate_imgs
+# from resolution.image_super_resolution import predict
+# from interpolation.interpolate import interpolate_imgs
+import yaml
 
 MSE_THRESH = 10
 LEADING_ZEROS = 5
@@ -75,14 +76,24 @@ def recombine(input_dir: str, output_dir: str, metadata):
         out.write(img_array[i])
     out.release()
 
-if __name__ == "__main__":
-    #WARNING: this stage assumes that the input directory only has 1 video.
-    #multiple videos will be combined - which may look strange
-    split_stage = file_stage("Split", "split/", split_video)
-    upscale_stage = stage("Upscale", "upscale/", upscale)
-    remove_cut_stage = stage("Remove Cuts", "output/", remove_cut)
-    recombine_stage = stage("Recombine", "video/", recombine)
 
-    pipe = pipeline("input.mp4", [split_stage, upscale_stage, remove_cut_stage, recombine_stage])
+split_stage = file_stage("Split", "split/", split_video)
+upscale_stage = stage("Upscale", "upscale/", upscale)
+remove_cut_stage = stage("Remove Cuts", "output/", remove_cut)
+recombine_stage = stage("Recombine", "video/", recombine)
+
+name_to_stage = {
+    "split": split_stage,
+    "upscale": upscale_stage,
+    "remove_cuts": remove_cut_stage,
+    "recombine": recombine_stage
+}
+
+if __name__ == "__main__":
+    with open("./config.yaml", 'r') as stream:
+        config = yaml.safe_load(stream)
+
+    stages = [name_to_stage[i] for i in config.get("stages")]
+    pipe = pipeline("input.mp4", stages)
     output_dir = pipe.execute(True, True)
 
